b30e89cf26cca4494b7af581cf528da4d9d508cb
Kyle McDonald <kyle@kylemcdonald.net>
2016-01-11 01:55:59 +0900

    basic functionality

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mkdirp = require('mkdirp');
+var fs = require('fs');
+
+app.use(express.static('public'));
+app.use(bodyParser.raw({ type: 'image/jpeg', limit: '10mb'}));
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
 
+app.post('/upload', function(req, res) {
+	console.log(req.query);
+	var cameraId = req.query.cameraId;
+	var dir = 'uploads/';
+	mkdirp(dir, function (err) {
+	    if (err) {
+	    	console.error(err);
+	    	return;
+	    }
+	    var timestamp = Math.floor((new Date() / 1000));
+		var filename = timestamp + '.jpg';
+		var path = dir + filename;
+		console.log('saving upload to: ' + path);
+		fs.writeFile(path, req.body, 'binary', function(err) {
+			if(err) console.log(err);
+			else console.log('saved upload');
+		});
+	});
+	res.sendStatus(200);
+})
+
 var server = app.listen(process.env.PORT || 3000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('App listening at http://%s:%s', host, port);
 });
